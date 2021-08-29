39d38bddba03d99a56fb49a04908325fbe91bc99
jaymes15 <jamesoluwatomisinomotosho@gmail.com>
2021-08-29 11:34:55 +0100

    chore: Add ci/cd pipeline

--- heroku_env.sh
+++ heroku_env.sh
@@ -1,12 +1,7 @@
 HEROKU_APP="crypcentra-ico"
 DEBUG=True
-DJANGO_SECRET_KEY="django-insecure-l#)^&_&=#0o0*iinf!q2m^-@y@i8se0uea+026#%-ssdjhlq"
-ALLOWED_HOST="*"
-SETTINGS_MODULE="app.settings_dev"
-
 
 heroku config:set DEBUG=$DEBUG --app $HEROKU_APP
-heroku config:set ALLOWED_HOSTS=$ALLOWED_HOST --app $HEROKU_APP
-heroku config:set SECRET_KEY=$DJANGO_SECRET_KEY --app $HEROKU_APP
-heroku config:set DJANGO_SETTINGS_MODULE=$SETTINGS_MODULE --app $HEROKU_APP
 
+heroku run python manage.py makemigrations --app $HEROKU_APP
+heroku run python manage.py migrate --app $HEROKU_APP
