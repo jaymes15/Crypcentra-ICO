d90ee8da7611b4218d4ec356022b8371c2969c31
jaymes15 <jamesoluwatomisinomotosho@gmail.com>
2022-01-25 12:23:40 +0100

    feat

--- app/core/models.py
+++ app/core/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from core.model_validators.validate_date import validate_date
 from core.utils import current_date
 
-
+print("-========")
 class Coin(models.Model):
     """Coin Model"""
 
